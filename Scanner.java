47e68351d44d822b4cb7120ff36bd771b1230ea9
MarianGuzman1829 <147306242+MarianGuzman1829@users.noreply.github.com>
2023-11-26 17:22:38 -0600

    Se realizo parte del codigo de Scanner, completarlo

--- Scanner.java
+++ Scanner.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class Scanner {
-    
+    private final String source;
+
+    private final List<Token> tokens = new ArrayList<>();
+
+    private static final Map<String, TipoToken> palabrasReservadas;
+    static {
+        palabrasReservadas = new HashMap<>();
+        palabrasReservadas.put("select", TipoToken.SELECT);
+        palabrasReservadas.put("from", TipoToken.FROM);
+        palabrasReservadas.put("distinct", TipoToken.DISTINCT);
+    }
+
+    Scanner(String source){
+        this.source = source + " ";
+    }
+    List<Token> scanTokens(){
+        int estado = 0;
+        char caracter = 0;
+        String lexema = "";
+        int inicioLexema = 0;
+    }
 }
