1438b9f35df428d839408ff2a17e43561c09093c
Atl Cardoso <atl1cardoso0@gmail.com>
2023-11-26 21:30:37 -0600

    Se realizaron los casos para crear los tokens

--- Scanner.java
+++ Scanner.java
@@ -13,13 +13,55 @@ public class Scanner {
         palabrasReservadas.put("distinct", TipoToken.DISTINCT);
     }
 
-    Scanner(String source){
+    Scanner(String source) {
         this.source = source + " ";
     }
-    List<Token> scanTokens(){
+
+    List<Token> scanTokens() {
         int estado = 0;
         char caracter = 0;
         String lexema = "";
         int inicioLexema = 0;
+
+        for (int i = 0; i < source.length(); i++) {
+            caracter = source.charAt(i);
+
+            switch (estado) {
+                case 0:
+                    if (caracter == '*') {
+                        tokens.add(new Token(TipoToken.ASTERISCO, "*", i + 1));
+                    } else if (caracter == ',') {
+                        tokens.add(new Token(TipoToken.COMA, ",", i + 1));
+                    } else if (caracter == '.') {
+                        tokens.add(new Token(TipoToken.PUNTO, ".", i + 1));
+                    } else if (Character.isAlphabetic(caracter)) {
+                        estado = 1;
+                        lexema = lexema + caracter;
+                        inicioLexema = i;
+                    }
+                    break;
+
+                case 1:
+                    if (Character.isAlphabetic(caracter) || Character.isDigit(caracter)) {
+                        lexema = lexema + caracter;
+                    } else {
+                        TipoToken tt = palabrasReservadas.get(lexema);
+                        if (tt == null) {
+                            tokens.add(new Token(TipoToken.IDENTIFICADOR, lexema, inicioLexema + 1));
+                        } else {
+                            tokens.add(new Token(tt, lexema, inicioLexema + 1));
+                        }
+
+                        estado = 0;
+                        i--;
+                        lexema = "";
+                        inicioLexema = 0;
+                    }
+                    break;
+            }
+        }
+        tokens.add(new Token(TipoToken.EOF, "", source.length()));
+
+        return tokens;
     }
 }
