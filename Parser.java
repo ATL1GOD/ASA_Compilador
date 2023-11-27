997390a8edb55d20e231ea2476bcbcf1e2fb7e2b
Atl Cardoso <atl1cardoso0@gmail.com>
2023-11-26 21:20:12 -0600

    Se agrego la tabla de analisis en base al automata

--- Parser.java
+++ Parser.java
@@ -1,72 +1,73 @@
 import java.util.*;
 
 public class Parser {
-    private final List<Token> tokens; 
-    private int i=0;
+    private final List<Token> tokens;
+    private int i = 0;
     private Token comparador;
-    private final Token ultimo= new Token(TipoToken.EOF,"");
+    private final Token ultimo = new Token(TipoToken.EOF, "");
     Stack<Integer> pila = new Stack<>();
 
-    public Parser (List<Token> tokens){
-        this.tokens=tokens; //constructor de la lista de tokens
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens; // constructor de la lista de tokens
     }
-    private static final String[][] Acciones ={
-        /*            */
-        /*Estado 0*/{"s2","",""},
-        /*Estado 1*/{},
-        /*Estado 2*/{},
-        /*Estado 3*/{},
-        /*Estado 4*/{},
-        /*Estado 5*/{},
-        /*Estado 6*/{},
-        /*Estado 7*/{},
-        /*Estado 8*/{},
-        /*Estado 9*/{},
-        /*Estado 10*/{},
-        /*Estado 11*/{},
-        /*Estado 12*/{},
-        /*Estado 13*/{},
-        /*Estado 14*/{},
-        /*Estado 15*/{},
-        /*Estado 16*/{},
-        /*Estado 17*/{},
-        /*Estado 18*/{},
-        /*Estado 19*/{},
-        /*Estado 20*/{},
-        /*Estado 21*/{},
-        /*Estado 22*/{},
-        /*Estado 23*/{},
-        /*Estado 24*/{},
+
+    private static final String[][] Acciones = {
+            /***********
+             * select | from | distinct | id | . | * | , | $ | Q | D | P | A | A1 | A2 | T |
+             * T1 | T2
+             */
+            /* Estado 0 */ { "s2", "", "", "", "", "", "", "", "1", "", "", "", "", "", "", "", "" },
+            /* Estado 1 */ { "", "", "", "", "", "", "", "acc", "", "", "", "", "", "", "", "", "" },
+            /* Estado 2 */ { "", "", "s3", "s8", "", "s5", "", "", "", "9", "4", "6", "7", "", "", "", "" },
+            /* Estado 3 */ { "", "", "", "s8", "", "s5", "", "", "", "", "16", "6", "7", "", "", "", "" },
+            /* Estado 4 */ { "", "r3", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 5 */ { "", "r4", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 6 */ { "", "r5", "", "", "", "", "s17", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 7 */ { "", "r7", "", "", "", "", "r7", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 8 */ { "", "r10", "", "", "s21", "", "r10", "", "", "", "", "", "", "20", "", "", "" },
+            /* Estado 9 */ { "", "s10", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 10 */ { "", "", "", "s13", "", "", "", "", "", "", "", "", "", "", "11", "12", "" },
+            /* Estado 11 */ { "", "", "", "", "", "", "s18", "r1", "", "", "", "", "", "", "", "", "" },
+            /* Estado 12 */ { "", "", "", "", "", "", "r12", "r12", "", "", "", "", "", "", "", "", "" },
+            /* Estado 13 */ { "", "", "", "s15", "", "", "r15", "r15", "", "", "", "", "", "", "", "", "14" },
+            /* Estado 14 */ { "", "", "", "", "", "", "r13", "r13", "", "", "", "", "", "", "", "", "" },
+            /* Estado 15 */ { "", "", "", "", "", "", "r14", "r14", "", "", "", "", "", "", "", "", "" },
+            /* Estado 16 */ { "", "r2", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 17 */ { "", "", "", "", "", "", "", "", "", "", "", "", "19", "", "", "", "" },
+            /* Estado 18 */ { "", "", "", "s8", "", "", "", "", "", "", "", "", "", "", "", "23", "" },
+            /* Estado 19 */ { "", "r6", "", "s13", "", "", "", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 20 */ { "", "r8", "", "", "", "", "r6", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 21 */ { "", "", "", "s22", "", "", "r8", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 22 */ { "", "r9", "", "", "", "", "r9", "", "", "", "", "", "", "", "", "", "" },
+            /* Estado 23 */ { "", "", "", "", "", "", "r11", "r11", "", "", "", "", "", "", "", "", "" },
+            /* Estado 24 */ { "", "", "", "", "", "", "", "acc", "", "", "", "", "", "", "", "", "" },
     };
 
-    public void parse(){
+    public void parse() {
         pila.push(0);
 
-        if(comparador.equals(ultimo)){
+        if (comparador.equals(ultimo)) {
             System.out.println("La entrada realizada, no es correcta o aceptable, intentelo nuevamente");
         }
 
-        else{
-            while(true){
-                comparador=tokens.get(i);
-                int estado=pila.peek();
-                int columna = columnaTerminal(comparador.tipo); //funcion
-                int estadoMovimiento=0;
-                String movimiento=Acciones[estado][columna];
-                if(movimiento.startsWith("s")){ //s = desplazamiento
-                    estadoMovimiento=Integer.parseInt(movimiento.substring(1));
+        else {
+            while (true) {
+                comparador = tokens.get(i);
+                int estado = pila.peek();
+                int columna = columnaTerminal(comparador.tipo); // funcion
+                int estadoMovimiento = 0;
+                String movimiento = Acciones[estado][columna];
+                if (movimiento.startsWith("s")) { // s = desplazamiento
+                    estadoMovimiento = Integer.parseInt(movimiento.substring(1));
                     pila.push(estadoMovimiento);
                     i++;
-                } 
-                else if (movimiento.startsWith("r")){ // r = reduccion = retroceso
-                    estadoMovimiento=Integer.parseInt(movimiento.substring(1));
-                    reduccion(estadoMovimiento);//funcion
-                }
-                else if (movimiento.equals("acc")){
+                } else if (movimiento.startsWith("r")) { // r = reduccion = retroceso
+                    estadoMovimiento = Integer.parseInt(movimiento.substring(1));
+                    reduccion(estadoMovimiento);// funcion
+                } else if (movimiento.equals("acc")) {
                     System.out.println("La consulta ingresada es correcta");
                     return;
-                }
-                else{ //error
+                } else { // error
                     System.out.println("La consulta ingresada es incorrecta, intentalo nuevamente");
                     return;
                 }
@@ -74,7 +75,7 @@ public class Parser {
         }
     }
 
-    private int columnaTerminal(TipoToken tipo){
+    private int columnaTerminal(TipoToken tipo) {
         String t = String.valueOf(tipo); // Convertimos el tipo de token en String para compararlo
         if (t.equals("SELECT"))
             return 0;
@@ -96,7 +97,7 @@ public class Parser {
             return 8; // ERROR
     }
 
-    void reduccion(int estadoAnterior){
+    void reduccion(int estadoAnterior) {
 
     }
 }
