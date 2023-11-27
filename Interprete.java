fae3c8bbea13b30b0b1f2ce31665c5049f54283f
Atl Cardoso <atl1cardoso0@gmail.com>
2023-11-26 21:27:22 -0600

    Se agregaron varias funciones como la de ejecucion y la de error

--- Interprete.java
+++ Interprete.java
@@ -7,23 +7,23 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Interprete {
-    static boolean existenErrores = false;
+  static boolean existenErrores = false;
 
-    public static void main(String[] args) throws IOException, Exception {
-        if (args.length > 1) {
-            System.out.println("Uso correcto: Interprete [archivo.txt]");
-            // condicional para error en el archivo de texto, nos ayudara a identificar como
-            // inicia el programa
-            // Convención definida en el archivo "system.h" de UNIX
-            System.exit(64); // status (palabra por defecto):64
-          } else if (args.length == 1) {
-            ejecutarArchivo(args[0]);
-          } else {
-            ejecutarPrompt();
-          }
+  public static void main(String[] args) throws IOException, Exception {
+    if (args.length > 1) {
+      System.out.println("Uso correcto: Interprete [archivo.txt]");
+      // condicional para error en el archivo de texto, nos ayudara a identificar como
+      // inicia el programa
+      // Convención definida en el archivo "system.h" de UNIX
+      System.exit(64); // status (palabra por defecto):64
+    } else if (args.length == 1) {
+      ejecutarArchivo(args[0]);
+    } else {
+      ejecutarPrompt();
     }
+  }
 
-private static void ejecutarArchivo(String path) throws IOException, Exception  {
+  private static void ejecutarArchivo(String path) throws IOException, Exception {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     ejecutar(new String(bytes, Charset.defaultCharset()));
     // Se indica que existe un error
@@ -31,4 +31,36 @@ private static void ejecutarArchivo(String path) throws IOException, Exception  
       System.exit(65); // status (palabra por defecto):65
   }
 
+  private static void ejecutarPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print(">>> ");
+      String linea = reader.readLine();
+      if (linea == null)
+        break; // Presionar Ctrl + D
+      ejecutar(linea);
+      existenErrores = false;
+    }
+  }
+
+  private static void ejecutar(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    Parser parser = new Parser(tokens);
+    parser.parse();
+
+  }
+
+  static void error(int linea, String mensaje) {
+    reportar(linea, "", mensaje);
+  }
+
+  private static void reportar(int linea, String donde, String mensaje) {
+    System.err.println(
+        "[linea " + linea + "] Error " + donde + ": " + mensaje);
+    existenErrores = true;
+  }
 }
